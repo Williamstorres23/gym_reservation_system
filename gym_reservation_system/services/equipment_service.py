ddb1f7ff08aeae6e26d24ed50413d10eb0ecdb80
Williams Torres Garcia da Silva <wtdesigner23@gmail.com>
2025-01-23 16:28:29 -0300

    Delete gym_reservation_system directory

--- gym_reservation_system/services/equipment_service.py
+++
@@ -1,9 +0,0 @@
-from dao.equipment_dao import EquipmentDAO
-from models.equipment import Equipment
-
-class EquipmentService:
-    @staticmethod
-    def create_equipment(equipment_data):
-        equipment = Equipment(**equipment_data)
-        EquipmentDAO.add_equipment(equipment)
-        return equipment
--- gym_reservation_system/services/user_service.py
+++
@@ -1,9 +0,0 @@
-from dao.user_dao import UserDAO
-from models.user import User
-
-class UserService:
-    @staticmethod
-    def create_user(user_data):
-        user = User(**user_data)
-        UserDAO.add_user(user)
-        return user
